5b432db1b2a943891d0526ffca7e3ce5090a5806
Bhavya <bhavyasri.tech.01@gmail.com>
2026-08-07 17:25:31 +0530

    final1

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://cyber-black-box.vercel.app/",  # replace with your actual Vercel URL once you have it
+        "https://cyber-black-box.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
